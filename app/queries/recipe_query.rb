bd4cdc67325dd9db68b165ed0dca402d822235fc
Ana Nunes da Silva <ananunesdasilva@gmail.com>
2024-10-01 22:11:33 +0100

    Improve search rules and relevancy

--- app/queries/recipe_query.rb
+++ app/queries/recipe_query.rb
@@ -8,7 +8,28 @@ class RecipeQuery
   def search(ingredients = [])
     return relation.none if ingredients.empty?
 
-    search_patterns = ingredients.map { |ingredient| "%#{ingredient}%" }
-    relation.where("array_to_string(ingredients, ',') ILIKE ANY (ARRAY[?])", search_patterns)
+    relation.where("array_to_string(ingredients, ' ') ~* ANY (ARRAY[?])", search_patterns(ingredients))
+            .select("recipes.*, #{matching_ingredients_count(ingredients)} AS matching_ingredients_count")
+            .order(Arel.sql('matching_ingredients_count DESC, rating DESC'))
+  end
+
+  private
+
+  def search_patterns(ingredients)
+    ingredients.flat_map do |ingredient|
+      pluralized_ingredient = ingredient.pluralize
+      singularized_ingredient = ingredient.singularize
+      [
+        "\\m#{Regexp.escape(singularized_ingredient)}\\M",
+        "\\m#{Regexp.escape(pluralized_ingredient)}\\M"
+      ]
+    end
+  end
+
+  def matching_ingredients_count(ingredients)
+    patterns = search_patterns(ingredients).map do |pattern|
+      "CASE WHEN array_to_string(ingredients, ' ') ~* '#{pattern}' THEN 1 ELSE 0 END"
+    end
+    "(#{patterns.join(' + ')})"
   end
 end
--- spec/queries/recipe_query_spec.rb
+++ spec/queries/recipe_query_spec.rb
@@ -18,6 +18,24 @@ RSpec.describe RecipeQuery do
                                      ingredients: ["1 cup broccoli", "1 cup bell pepper", "1 tablespoon soy sauce",
                                                    "1 tablespoon sesame oil"]) }
 
+    let!(:recipe4) { create(:recipe, title: "Coconut Curry", cook_time: 30, prep_time: 10, rating: 4.8,
+                                     image_link: "https://example.com/image4.jpg",
+                                    author: "curryking",
+                                    ingredients: ["1 can coconut milk", "1 cup chickpeas", "1 cup spinach",
+                                                  "2 tablespoons curry powder", "1 tablespoon olive oil",
+                                                  "1 teaspoon cumin", "1 teaspoon turmeric", "1/2 cup diced tomato",
+                                                  "Salt to taste"])}
+
+    let!(:recipe5) { create(:recipe, title: "Buttermilk Pancakes", cook_time: 15, prep_time: 10, rating: 4.9,
+                                     image_link: "https://example.com/image_buttermilk.jpg",
+                                     author: "pancakequeen",
+                                     ingredients: ["2 cups all-purpose flour", "2 tablespoons sugar",
+                                                   "1 teaspoon baking powder", "1 teaspoon baking soda",
+                                                   "1/2 teaspoon salt", "2 cups buttermilk",
+                                                   "2 large eggs", "1/4 cup melted butter",
+                                                   "1 teaspoon vanilla extract"]) }
+
+
     let(:recipe_query) { described_class.new }
 
     context 'when no ingredients are provided' do
@@ -31,24 +49,62 @@ RSpec.describe RecipeQuery do
       it 'returns recipes that match the given ingredients' do
         recipes = recipe_query.search(["chicken"])
         expect(recipes).to include(recipe2)
-        expect(recipes).not_to include(recipe1, recipe3)
+        expect(recipes).not_to include(recipe1, recipe3, recipe4, recipe5)
+      end
+
+      it 'is case insensitve' do
+        recipes = recipe_query.search(["Chicken"])
+        expect(recipes).to include(recipe2)
+        expect(recipes).not_to include(recipe1, recipe3, recipe4, recipe5)
       end
 
       it 'returns more than one matching recipe for a single ingredient' do
         recipes = recipe_query.search(["salt"])
-        expect(recipes).to include(recipe1, recipe2)
+        expect(recipes).to include(recipe1, recipe2, recipe4, recipe5)
         expect(recipes).not_to include(recipe3)
       end
 
       it 'returns recipes that match multiple ingredients' do
-        recipes = recipe_query.search(["corn", "garlic"])
+        recipes = recipe_query.search(["cornmeal", "garlic"])
+
         expect(recipes).to include(recipe1, recipe2)
-        expect(recipes).not_to include(recipe3)
+        expect(recipes).not_to include(recipe3, recipe4, recipe5)
       end
 
       it 'returns an empty collection if no recipes match the ingredients' do
         recipes = recipe_query.search(["nonexistent ingredient"])
         expect(recipes).to be_empty
+      end
+    end
+
+    context 'composed ingredients containing words of other ingredients' do
+      it 'does the exact match of ingredients (milk is not buttermilk)' do
+        recipes = recipe_query.search(["milk"])
+
+        expect(recipes).to include(recipe1, recipe4)
+        expect(recipes).not_to include(recipe2, recipe3, recipe5)
+      end
+    end
+
+    context 'ingredients with more than one word' do
+      it 'does the exact match of the ingredients' do
+        recipes = recipe_query.search(["coconut milk"])
+        expect(recipes).to include(recipe4)
+        expect(recipes).not_to include(recipe1, recipe2, recipe3, recipe5)
+      end
+    end
+
+    context 'when searching with relevancy ordering' do
+      it 'returns recipes ordered by the count of the searched ingredients included in the recipe' do
+        recipes = recipe_query.search(["buttermilk", "flour"])
+        expect(recipes).to eq([recipe5, recipe1])
+      end
+
+      context 'when count of ingredients is the same' do
+        it 'returns recipes ordered by rating' do
+          recipes = recipe_query.search(["salt", "flour"])
+          expect(recipes).to eq([recipe5, recipe1, recipe4, recipe2])
+        end
       end
     end
   end
